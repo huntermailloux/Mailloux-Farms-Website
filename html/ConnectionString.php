b08fc686762813c749084171c92f79a20297db48
Hunter Mailloux <huntermmailloux@gmail.com>
2023-04-09 13:10:44 -0400

    Progress after Provincials Comp

---
+++ html/ConnectionString.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_host = 'localhost'; 
+$db_username = 'maillo51_maillouxFarms'; 
+$db_password = 'Mailloux123'; 
+$db_name = 'maillo51_db'; 
+
+
+$conn = new mysqli($db_host, $db_username, $db_password, $db_name);
+
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+?>
